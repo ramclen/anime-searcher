cc0b60a750a1f44e0503c1508ffd61c5cc7e21e8
Cristopher <cristopher.correo@gmail.com>
2020-06-01 18:16:59 +0100

    creating the first card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="card bg-dark text-white">
+              <img src="https://media.kitsu.io/anime/poster_images/6448/medium.jpg?1431828590" class="card-img" alt="anime" />
+              <div class="card-img-overlay">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+
+  }
 }
-
-export default App;
