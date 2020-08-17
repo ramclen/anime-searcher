ee6da2c8272b45cde159c5eee248d2e1104d6c05
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-17 20:08:58 +0000

    Deploy ramclen/anime-searcher to github.com/ramclen/anime-searcher.git:gh-pages

---
+++ precache-manifest.1ab2436e0e56880d93775605b061dbf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf449eb921810fb71e815a6ce17ea8a",
+    "url": "/anime-searcher/index.html"
+  },
+  {
+    "revision": "dba6502ad2faa924bc37",
+    "url": "/anime-searcher/static/css/main.afbdd3bd.chunk.css"
+  },
+  {
+    "revision": "7c173cb62adb3a3df933",
+    "url": "/anime-searcher/static/js/2.b0fd5aee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anime-searcher/static/js/2.b0fd5aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba6502ad2faa924bc37",
+    "url": "/anime-searcher/static/js/main.4b3fb965.chunk.js"
+  },
+  {
+    "revision": "e97d41f3c59efa1a24ad",
+    "url": "/anime-searcher/static/js/runtime-main.8aabf5b8.js"
+  }
+]);
